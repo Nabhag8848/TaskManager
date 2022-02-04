93f818c6093e26b4e8665e60f4959bdc43390e78
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-02-04 11:30:36 +0530

    Learnt:Updating Documents (methods:updateOne,updateMany)

--- mongodb.js
+++ mongodb.js
@@ -10,109 +10,22 @@ MongoClient.connect(connectionURL, { useNewURLParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-
-    //fetches first document which matches the filter
-    db.collection('users').findOne({age:19},(error,user)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(user)
+    
+    db.collection('users').updateOne({_id: new ObjectId("61fa29da919587dc7adb7e9a")},
+    {
+    $inc:{
+        age:-1
+    }}).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 
-    //fetches document as per to filter,Grabing by id
-    db.collection('users').findOne({_id: new ObjectId("61fa29da919587dc7adb7e9a"),age:19},(error,user)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(user)
+    db.collection('tasks').updateMany({completed:true},{$set:{
+        completed:false
+    }}).then((result) =>{
+        console.log(result)
+    }).catch((error)=>{
+        console.log(error)
     })
-
-    //fetches all the users having age 19
-    db.collection('users').find({age:19}).toArray((error,users)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(users)
-
-    })
-
-    //returns number of documents matching filter
-    db.collection('users').find({age:19}).count((error,users)=>{
-
-        if(error){
-            return console.log('Unable to count')
-        }
-
-        console.log(users)
-
-    })
-
-    //fetches all tasks which aren't complete
-    db.collection('tasks').find({completed:false}).toArray((error,tasks)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(tasks)
-
-    })
-
-    //fetching first task which matches the filter
-    db.collection('tasks').findOne({completed:true},(error,task)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(task)
-    })    //fetches first document which matches the filter
-    db.collection('users').findOne({age:19},(error,user)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(user)
-    })
-
-    //fetches document as per to filter,Grabing by id
-    db.collection('users').findOne({_id: new ObjectId("61fa29da919587dc7adb7e9a"),age:19},(error,user)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(user)
-    })
-
-    //fetches all the users having age 19
-    db.collection('users').find({age:19}).toArray((error,users)=>{
-
-        if(error){
-            return console.log('Unable to fetch')
-        }
-
-        console.log(users)
-
-    })
-
-    //returns number of documents matching filter
-    db.collection('users').find({age:19}).count((error,users)=>{
-
-        if(error){
-            return console.log('Unable to count')
-        }
-
-        console.log(users)
-
-    })
-
 })
-
