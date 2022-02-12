efca780b4c56281da36d7cd1a1edeff9e9e8f8cf
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-02-12 17:31:42 +0530

    learnt: creating reading resources endpoints for task(s) and user(s).

--- src/index.js
+++ src/index.js
@@ -17,12 +17,57 @@ app.post('/users',(req,res)=>{
     })
 })
 
+app.get('/users',(req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id',(req,res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+
+        if(user){
+            return res.send(user)
+        }
+
+        res.status(404).send()
+
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks',(req,res) => {
     const task = new Task(req.body)
     task.save().then(()=>{
         res.status(201).send(task)
     }).catch((e)=>{
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks',(req,res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id',(req,res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+
+        if(task){
+            return res.send(task)
+        }
+
+        res.status(404).send()
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
