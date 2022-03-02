303d70b5e6e698ed07443c6c6dea6f558c17a68e
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-03-02 21:36:54 +0530

    fix: configure the deleteTaskandCount and updateAgeandCount with async-await.

--- playground/1-promises-with-mongoose.js
+++ playground/1-promises-with-mongoose.js
@@ -2,6 +2,7 @@ require('../src/db/mongoose')
 const User = require('../src/models/user')
 
 const id = '620511dcb6368e75c20196eb'
+//without using async await
 User.findByIdAndUpdate(id, {age : 19}).then((userWithUpdate) => {
     console.log(userWithUpdate)
     return User.countDocuments({age: 19})
@@ -11,3 +12,15 @@ User.findByIdAndUpdate(id, {age : 19}).then((userWithUpdate) => {
     console.log(e)
 })
 
+const updateAgeAndCount = async (id,age) => {
+
+    const user = await User.findByIdAndUpdate(id,{age})
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount(id, 21).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
--- playground/2-promises-with-mongoose.js
+++ playground/2-promises-with-mongoose.js
@@ -1,11 +1,26 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-const id = '6200af36006db68df1bb068e'
+const id = '62052fe8033ae58b376fd858'
+
+//without using async-await
 Task.findByIdAndDelete(id).then(() => {
     return Task.countDocuments({completed:false})
 }).then((incompleteTask) => {
     console.log(incompleteTask)
+}).catch((e) => {
+    console.log(e)
+})
+
+const deleteTaskAndCount = async (id) => {
+
+    const task = await Task.findByIdAndDelete(id)
+    const incompleteTask = await Task.countDocuments({ completed: false })
+    return incompleteTask
+}
+
+deleteTaskAndCount(id).then((countOfIncompleteTask) => {
+    console.log(countOfIncompleteTask)
 }).catch((e) => {
     console.log(e)
 })
