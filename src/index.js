cb27e2b3428f2472f25bfcb738ccf19ca5070719
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-03-09 19:25:26 +0530

    feat: setup an authentication to route GET /users/me which was earlier GET /users
    description: so what i have done is when user logins,from client the token has to pass into headers now with jwt we will verify the token is valid with secret and if authenticated we will let router handler run else will throw an Error 401 also reason for changing the route was when user authenticated for eg we never wanna share all the emails of people logged in so instead we have Repurpose it to Read Profile of Authenticated User.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+    
+//     if(req.method === 'GET'){
+//         res.send('GET request restricted!!')
+//     }else{
+//         next()
+//     }
+
+// })
+
+//Setting up middleware for site Under maintenance
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is under maintanence try again soon.')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
