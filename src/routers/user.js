16a054628d5ca90b84937cca7caffc63d29884d5
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-03-16 17:22:57 +0530

    feat: setup an authentication to route PATCH users/me (earlier users/:id) and DELETE users/me (earlier users/:id)
    description: override toJSON for userSchema so we can restrict to not send the valuable info of user which should not expose publically reason for this was when response is send toJSON will be called implicitly with JSON.stringify(). reason for changing the routes path is we never need user to delete/update other user info if they know the id so changed to /me so only they can only delete/update their own profile.

--- src/routers/user.js
+++ src/routers/user.js
@@ -77,7 +77,7 @@ router.get('/users/:id',async (req,res) => {
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
 
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -87,34 +87,20 @@ router.patch('/users/:id', async (req, res) => {
         return res.status(406).send({error: 'Invalid Updates!!'})
     }
 
-    const _id = req.params.id
     try {
-
-        const user = await User.findById(_id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-        
-        if(user){
-            return res.send(user)
-        }
-
-        res.status(404).send()
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     }catch (e){
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     
-    const _id = req.params.id
     try{
-        const user = await User.findByIdAndDelete(_id)
-        
-        if(user){
-            return res.send(user)
-        }
-
-        res.status(404).send()
+        req.user.remove()
+        res.send()
     }catch (e){
         res.status(500).send(e)
     }   
